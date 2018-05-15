5244ae3f6ba510fec73696019980fbbbcd2ef448
justsenne <senne1320@gmail.com>
2018-05-15 11:06:01 +0200

    game

---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Startup : MonoBehaviour {
+    
+    public static int[] GetCode()
+    {
+        var Code = new int[4];
+        for (int x = 0; x > 4; x++)
+        {
+            Code[x] = Random.Range(0, 4);
+        };
+        return Code;
+    }
+
+    
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
