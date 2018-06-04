014e8fa71b3653d86a8f81251f68116ade0bb56f
justsenne <senne1320@gmail.com>
2018-06-04 13:18:29 +0200

    newColor.cs

---
+++ Assets/newColor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newColor : MonoBehaviour {
+
+    public Material[] materials;
+    public Renderer rend;
+    private int index = 1;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+
+    void OnMouseDown()
+    {
+        if (materials.Length == 0)
+        {
+            return;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            index = 1;
+        }
+        rend.sharedMaterial = materials[index - 1];
+    }
+}
