62dc1f3b0da2259da1e08c5ec4edc5cb9f96ff98
J_Aydin <janberd.aydin@gmail.com>
2018-06-04 11:07:26 +0200

    volumehehe

---
+++ Assets/changeVolume.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeVolume : MonoBehaviour {
+
+    public Slider Volume;
+    public AudioSource backgroundMusic;
+	
+	// Update is called once per frame
+	void Update () {
+        backgroundMusic.volume = Volume.value;
+	}
+}
