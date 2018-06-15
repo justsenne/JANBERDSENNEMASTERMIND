62418436c7a82079c64f752313670ae5dfa2534e
J_Aydin <janberd.aydin@gmail.com>
2018-06-15 16:05:33 +0200

    JADATWEETIKZEKER

---
+++ Assets/changeColour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeColour : MonoBehaviour
+{
+    private Button theButton;
+    private ColorBlock theColor;
+    int Value = 0;
+
+    Color[] arrayColors = new Color[6];
+
+    void Awake()
+    {
+        arrayColors[0] = Color.red;
+        arrayColors[1] = Color.blue;
+        arrayColors[2] = Color.green;
+        arrayColors[3] = Color.yellow;
+        arrayColors[4] = Color.black;
+        arrayColors[5] = Color.magenta;
+
+        theButton = GetComponent<Button>();
+        theColor = GetComponent<Button>().colors;
+    }
+
+    public void changeThemColours()
+    {
+        theColor.highlightedColor = arrayColors[Value];
+        theColor.normalColor = arrayColors[Value];
+
+        theButton.colors = theColor;
+        if (Value < 7)
+        {
+            Value++;
+        }
+        else
+        {
+            Value = 0;
+        }
+        
+    }
+}
